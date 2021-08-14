a9c5743b86bd30549c14915660663f6886517f7e
Conrad Zelck <git@simpel.cc>
2021-08-14 17:20:02 +0000

    fix: delete .txt file every round
    DuckDuckGo for instance creates a file .txt. That can be accessed w/o
    timeout. For privacy reasons a username had to be created with it's
    file username.txt. That .txt had to be deleted immediately but isn't
    found with glob('*.txt'). So it must be deleted explicitely.
    
    close #4
    
    Signed-off-by: Conrad Zelck <git@simpel.cc>

--- delete.php
+++ delete.php
@@ -2,11 +2,14 @@
   delete();
   sleep(8);
   delete();
-  
+
   function delete() {
+    // Sometimes a .txt is created - delete it immediately
+    unlink(".txt");
+    // delete all *.txt older then 7 seconds
     $expire = strtotime('-7 SECONDS');
     $files = glob('*.txt');
-    print_r($files);
+    // print_r($files);
     foreach ($files as $file) {
       // Skip anything that is not a file
       if (!is_file($file)) {
