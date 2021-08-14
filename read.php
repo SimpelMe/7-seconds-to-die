34cd8718d33a0ac818be98e1950fa73c78a0894b
Conrad Zelck <git@simpel.cc>
2021-08-14 17:51:25 +0000

    fix: don't even write .txt file
    If no username is existing and it tries to create a .txt file after
    pasting delete it instantly.
    
    see #4
    
    Signed-off-by: Conrad Zelck <git@simpel.cc>

--- read.php
+++ read.php
@@ -2,4 +2,6 @@
   $file = preg_replace('/\W/','', $_POST["user"]) . '.txt';
   $data = $_POST["text"];
   file_put_contents($file, $data, LOCK_EX);
+  // Sometimes a .txt (w/o user) is created - delete it immediately
+  unlink(".txt");
 ?>
