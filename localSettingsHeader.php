aeb6ecb8a6b11a60d01bef7fd46c123fddce4788
Conrad Zelck <git@simpel.cc>
2022-04-14 15:03:36 +0200

    fix: correct h1 word 'seconds'
    Signed-off-by: Conrad Zelck <git@simpel.cc>

--- localSettingsHeader.php
+++ localSettingsHeader.php
@@ -1,6 +1,6 @@
 <?php
   define("SITE_TITLE", "7 sec to die");
   define("SITE_COPYRIGHT_CONTENT", "MIT 2021 Simpel");
-  define("SITE_H1", "7 sec to die");
+  define("SITE_H1", "7 seconds to die");
   define("SITE_GITHUB", "https://github.com/SimpelMe/7-seconds-to-die");
 ?>
