0533d364f6c16e3c3db9ba0e78bdd93c90c54c5e
Conrad Zelck <git@simpel.cc>
2023-11-11 14:35:24 +0100

    feat: hash 4-words-phrase
    Secrets shouldn't be visible at all. Even not as a cookie. So hash the
    secret and use this as new password. To give a visual hint what secret
    is used take the hash and omit every second character. This show as user.
    
    Signed-off-by: Conrad Zelck <git@simpel.cc>

--- functions.js
+++ functions.js
@@ -14,23 +14,42 @@ function decrypt(message) {
   return string;
 }
 
+function cr32enc(r) {
+  for (var a, o = [], c = 0; c < 256; c++) {
+    a = c;
+    for (var f = 0; f < 8; f++) a = 1 & a ? 3988292384 ^ a >>> 1 : a >>> 1;
+    o[c] = a
+  }
+  for (var n = -1, t = 0; t < r.length; t++) n = n >>> 8 ^ o[255 & (n ^ r.charCodeAt(t))];
+  return (-1 ^ n) >>> 0
+}
+
+function smallHash(text) {
+  var cr32 = cr32enc(text);
+  var encoded = cr32.toString(32);
+  return encoded;
+}
+
 function setPhrase() {
-  makePhrase();
+  makePhrase() // create phrase with phrase.js
   var userprompt = "";
   while (userprompt === "") {
     userprompt = prompt("Enter a phrase.\nValid characters are: a-Z, 0-9, - and _.\n\nProposal:\n", phrase);
     userprompt = userprompt.replace(/[^\-\w\d]/gi, ''); // nur a-Z, 0-9 und _
     userprompt = userprompt.trim(); // deletes trailing and leading whitespaces
+    userprompt = userprompt.toLowerCase(); // only small letters
     if (userprompt == null) { // kein user eingegeben
       userprompt = "";
     }
   }
   phrase = userprompt;
-  document.getElementById("reset").innerText = "Reset phrase '" + phrase + "'";
-  setCookie("Phrase", phrase, 7); // 7 days is maximum set by safari and brave
-  var middle = Math.floor(phrase.length / 2);
-  user = phrase.substr(0, middle + 1);
-  password = phrase.substr(middle);
+  phrase = smallHash(phrase);
+  setCookie("phrase", phrase, 7); // 7 days is maximum set by safari and brave
+  // username should be the phrase but every second character omitted
+  // This way you won't see the password in the gui on "reset" button
+  user = phrase.split('').filter((c, i) => (i + 1) % 2 !== 0).join('');
+  document.getElementById("reset").innerText = "Reset user '" + user + "'";
+  password = phrase;
 }
 
 function setCookie(cname, cvalue, exdays) {
@@ -57,21 +76,20 @@ function getCookie(cname) {
 }
 
 function checkCookie() {
-  var tempPhrase = getCookie("Phrase");
+  var tempPhrase = getCookie("phrase");
   if (tempPhrase != "") {
     phrase = tempPhrase;
-    document.getElementById("reset").innerText = "Reset phrase '" + phrase + "'";
-    setCookie("Phrase", phrase, 7); // set again after each reload as 7 days is maximum set by safari and brave
+    setCookie("phrase", phrase, 7); // set again after each reload as 7 days is maximum set by safari and brave
   } else {
     setPhrase()
   }
-  var middle = Math.floor(phrase.length / 2);
-  user = phrase.substr(0, middle + 1);
-  password = phrase.substr(middle);
+  user = phrase.split('').filter((c, i) => (i + 1) % 2 !== 0).join('');
+  document.getElementById("reset").innerText = "Reset user '" + user + "'";
+  password = phrase;
 }
 
 function deleteCookies() {
-  document.cookie = "Phrase=; expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
+  document.cookie = "phrase=; expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
 }
 
 function deleteUserCredentials() {
