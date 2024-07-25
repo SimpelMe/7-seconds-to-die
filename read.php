3d63cdb6b98a4e7558ff292f2bb50a8f0598f693
Conrad Zelck <git@simpel.cc>
2024-07-25 15:17:21 +0200

    fix read.php: check if user is alphanumeric
    You could have created files outside the wanted folder:
    curl -X "POST" "https://simpel.cc/7-seconds-to-die/read.php" \
         -H 'Content-Type: application/x-www-form-urlencoded; charset=utf-8' \
         --data-urlencode "user=../hallo-welt" \
         --data-urlencode "text=Hi!"
    
    creates https://simpel.cc/hallo-welt.txt
    
    Now the check for alphanumeric only avoids this.
    
    Signed-off-by: Conrad Zelck <git@simpel.cc>

--- read.php
+++ read.php
@@ -1,7 +1,11 @@
 <?php
-  $file = $_POST["user"] . '.txt';
+  $user = $_POST["user"];
+  $file = $user . '.txt';
   $data = $_POST["text"];
-  file_put_contents($file, $data, LOCK_EX);
+  // allow only alphanumeric to avoid creating file outside this folder
+  if (ctype_alnum($user)) {
+    file_put_contents($file, $data, LOCK_EX);
+  }
   // Sometimes a .txt (w/o user) is created - delete it immediately
   unlink(".txt");
 ?>
